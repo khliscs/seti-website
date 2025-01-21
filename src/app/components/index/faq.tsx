a1294c9452ab0438f338093f0948f8c2bd9e43d2
Kilian Hliscs <hliscs.k@gmail.com>
2025-01-21 23:47:15 +0100

    pt

--- src/app/components/index/faq.tsx
+++ src/app/components/index/faq.tsx
@@ -14,7 +14,7 @@ export default function FaqAccordion() {
     <div className={''}>
       <div className={'pb-4'}>
         <Accordion open={open === 1} placeholder={""}>
-          <AccordionHeader onClick={() => handleOpen(1)} placeholder={""} className={'text-white font-special '}>What is Material Tailwind?</AccordionHeader>
+          <AccordionHeader onClick={() => handleOpen(1)} placeholder={""} className={'text-white font-special text-3xl'}>What is Material Tailwind?</AccordionHeader>
           <AccordionBody className={'text-white'}>
             We&apos;re not always in the position that we want to be at. We&apos;re constantly
             growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
@@ -24,7 +24,7 @@ export default function FaqAccordion() {
       </div>
       <div className={'pb-4'}>
         <Accordion open={open === 2} placeholder={""}>
-          <AccordionHeader onClick={() => handleOpen(2)} placeholder={""} className={'text-white font-special'}>
+          <AccordionHeader onClick={() => handleOpen(2)} placeholder={""} className={'text-white font-special text-3xl'}>
             How to use Material Tailwind?
           </AccordionHeader>
           <AccordionBody className={'text-white'}>
@@ -36,7 +36,7 @@ export default function FaqAccordion() {
       </div>
       <div className={'pb-4'}>
         <Accordion open={open === 3} placeholder={""}>
-          <AccordionHeader onClick={() => handleOpen(3)} placeholder={""} className={'text-white font-special'}>
+          <AccordionHeader onClick={() => handleOpen(3)} placeholder={""} className={'text-white font-special text-3xl'}>
             What can I do with Material Tailwind?
           </AccordionHeader>
           <AccordionBody className={'text-white'}>
